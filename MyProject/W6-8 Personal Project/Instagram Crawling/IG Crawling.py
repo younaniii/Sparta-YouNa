c4877311936576746527d5a0157c4e9e2c2d4ceb
YouNa <you3634@gmail.com>
2020-04-26 00:30:17 +0900

    Personal Project_2
    - Added script: ajax GET, POST
    - Python file: backend

--- MyProject/W6-8 Personal Project/Instagram Crawling/IG Crawling.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from urllib.parse import quote_plus
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import time
-
-https://www.instagram.com/explore/tags/%EB%9E%9C%EC%84%A0%EC%97%AC%ED%96%89/
