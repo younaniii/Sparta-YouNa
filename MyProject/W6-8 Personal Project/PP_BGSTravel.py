e1e32e445836d0bf7993472b31e3567dbe8479e0
YouNa <you3634@gmail.com>
2020-04-26 23:12:57 +0900

    Personal Project_3
    - Split html into 2 files: home & saved bucketlist

--- MyProject/W6-8 Personal Project/PP_BGSTravel.py
+++ MyProject/W6-8 Personal Project/PP_BGSTravel.py
@@ -1,33 +1,43 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-import requests
+from urllib.request import urlopen
+from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+import time
 
+# pymongo를 임포트 하기(패키지 인스톨 먼저)
 from pymongo import MongoClient
+# mongoDB는 27017 포트로 돌아갑니다.
 client = MongoClient('localhost', 27017)
-db = client.PP_BangguseokTravel
+# 'BangguseokTravel'라는 이름의 db를 만듭니다.
+db = client.BangguseokTravel
 
-## HTML을 주는 부분
+
+# Home HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('PP_BGSTravel_Home.html')
 
-@app.route('/home', methods = ['GET'])
-def listing():
-    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    result = list(db.bucketlist.find({}))
+@app.route('/mybucketlist')
+def mybucketlist():
+    return render_template('PP_BGSTravel_MyBucketlist.html')
 
-    # 2. articles라는 키 값으로 포스트정보 내려주기
-    return jsonify({'result':'success',
-                    'msg':'저장한 포스트를 가져왔습니다:)',
-                    'bucketlist':result})
 
-## API 역할을 하는 부분
+# Crawling으로 인스타그램에서 사진을 가져오는 부분
+# API를 사용하자!
+
+# API 역할을 하는 부분: 마음에 드는 포스트를 저장하는 부분
 @app.route('/home', methods = ['POST'])
 def saving():
     # 1. 클라이언트로부터 데이터를 받기
     url = request.form['url_give']
+    image = request.form['image_give']
+    continent = request.form['continent_give']
+    country = request.form['country_give']
+    city = request.form['city_give']
     comment = request.form['comment_give']
 
     print(url)
@@ -39,7 +49,7 @@ def saving():
 
     #이 부분 어떻게 채워넣지?
 
-    bucketlist = {'keyword':keyword, 'image':image}
+    bucketlist = {'url':url, 'image':image, 'continent':continent, 'country':country, 'city':city, 'comment':comment}
 
     print(bucketlist)
 
@@ -47,6 +57,19 @@ def saving():
     db.bucketlist.insert_one(bucketlist)
 
     return jsonify({'result':'success', 'msg':'버킷리스트를 추가했습니다:D'})
+
+
+# 내가 저장한 포스트를 불러오는 부분
+@app.route('/home', methods = ['GET'])
+def listing():
+    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
+    result = list(db.bucketlist.find({}))
+
+    # 2. articles라는 키 값으로 포스트정보 내려주기
+    return jsonify({'result':'success',
+                    'msg':'저장한 포스트를 가져왔습니다:)',
+                    'bucketlist':result})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ MyProject/W7/sparta-w4/sparta-w4/app.py
@@ -0,0 +1,58 @@
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+import requests
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+
+# client = MongoClient('mongodb://sampleID:samplePW@12.34.56.78', 27017)
+client = MongoClient('mongodb://younanii:Younanii1217@15.164.100.108', 27017)
+db = client.dbsparta
+
+# HTML을 주는 부분
+@app.route('/')
+def home():
+    return 'This is Home!'
+
+
+@app.route('/mypage')
+def mypage():
+    return render_template('index.html')
+
+
+# API 역할을 하는 부분
+@app.route('/post', methods=['POST'])
+def post():
+    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
+    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    url_image = og_image['content']
+    url_title = og_title['content']
+    url_description = og_description['content']
+
+    article = {'url': url_receive, 'comment': comment_receive,
+               'image': url_image, 'title': url_title, 'desc': url_description}
+
+    db.articles.insert_one(article)
+
+    return jsonify({'result': 'success'})
+
+
+@app.route('/post', methods=['GET'])
+def view():
+    posts = db.articles.find({}, {'_id': 0})
+    return jsonify({'result': 'success', 'articles': list(posts)})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
